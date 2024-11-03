6e3a401e34cc9a4710c59a890b6c5aa3ca9414ed
Egor1408 <egor1408@yandex.ru>
2024-11-03 14:38:14 +0300

    router

--- backend/articles/article.router.js
+++ backend/articles/article.router.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 import ArticleController from "./article.controller.js";
 const router = new Router();
 
-router.post('/', ArticleController.createArticle);
-router.get('/', ArticleController.getArticleList);
-router.get('/:id', ArticleController.getArticle);
-router.put('/', ArticleController.updateArticle);
-router.delete('/:id', ArticleController.deleteArticle);
+router.post('/articles', ArticleController.createArticle);
+router.get('/articles', ArticleController.getArticleList);
+router.get('/articles/:id', ArticleController.getArticle);
+router.put('/articles', ArticleController.updateArticle);
+router.delete('/articles/:id', ArticleController.deleteArticle);
 
 export default router;
