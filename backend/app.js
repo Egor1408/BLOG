76cd656afea0ca368950dbab5716df37f12c6667
Egor1408 <egor1408@yandex.ru>
2024-11-11 20:30:56 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,20 @@ import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
 import router from './articles/article.router.js';
 
-const PORT = 5000;
+const PORT = 8080;
 const DB_URL = "mongodb://127.0.0.1:27017/mydb";
 
 const app = express();
 
 app.use(express.json());
 app.use(fileUpload());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', `http://localhost:4200`);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 app.use('/', router);
 startApp();
 
@@ -17,7 +24,7 @@ async function startApp() {
     try {
         await mongoose.connect(DB_URL);
         app.listen(PORT, () => {
-            console.log('server start on' , '\x1b[36m', `http://localhost:5000/`);
+            console.log('server start on' , '\x1b[36m', `http://localhost:${PORT}`);
         })
     } catch (err) {
         console.log(err);
--- backend/articles/article.schema.js
+++ backend/articles/article.schema.js
@@ -1,11 +1,30 @@
 import mongoose from "mongoose";
 
 const Article = new mongoose.Schema({
-    author: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    imageUrl: {type: String},
-    createdAt: {type: String, required: true},
+    author: {
+        type: String,
+        default: "Author",
+        required: true
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    imageUrl: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+},
+{
+    versionKey: false,
 });
 
 export default mongoose.model('Article', Article);
