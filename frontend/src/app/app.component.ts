06b84506f84a0e245843b508299a1d07e67010ad
Egor1408 <egor1408@yandex.ru>
2024-12-03 16:22:56 +0300

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HeaderComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'frontend';
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layouts/home/home.component';
+import { SigninComponent } from './layouts/signin/signin.component';
+import { SignupComponent } from './layouts/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "/", component: HomeComponent},
+  {path: "/signin", component: SigninComponent},
+  {path: "/signup", component: SignupComponent},
+  { path: "**", redirectTo: "/"},
+  // {path: "", component: HomeComponent},
+];
